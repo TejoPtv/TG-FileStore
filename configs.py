2461b87267be20753d9a74527fec9120bc9f78aa
TejoPtv <134220546+TejoPtv@users.noreply.github.com>
2023-07-04 20:28:51 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,5 +17,6 @@ class Config(object):
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS").split()))
-	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE",False))
+	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE",True))
 	ABOUT_BOT_TEXT = "Hi I am a file store bot For only Nothing boy 
+	i well provide Cartoons and movies in multi audio and audio 
